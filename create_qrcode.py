c4f746430002168d4eb49fc9093c88c5e38cecac
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2023-06-11 09:51:53 -0300

    Update create_qrcode.py

--- create_qrcode.py
+++ create_qrcode.py
@@ -3,10 +3,10 @@ import png
 from pyqrcode import QRCode
 
 #Link para criação do QRCode
-QRString = 'https://drive.google.com/file/d/1PNL3BD7FbULNZfmuD-XgEuWLAHSpnjlJ/view?usp=share_link'
+QRString = 'https://drive.google.com/file/d/1fC8l6PTXg-gMV9P7IBuvJIetshHrD7l2/view?usp=sharing'
 
 #Geração do QRCode
 url = pyqrcode.create(QRString)
 
 #Salva o QRCode gerado
-url.png(r'qr.png', scale=8)
+url.png(r'QR-Code_06-2023.png', scale=8)
